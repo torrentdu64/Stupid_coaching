95841c3148dadcc8e34ff30f5b28b5cb9de189b4
torrent benoit <torrentdu64@gmail.com>
2016-02-15 12:06:01 +0100

    generate coaching controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'coaching/answer'
+
+  get 'coaching/ask'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
